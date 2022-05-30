47888cc37575de6f96b32729ff06e8ffd9a3fc4a
aLe3ouLa <ale3mp90@gmail.com>
2022-05-30 19:08:11 +0200

    fix: deps

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,8 +1,5 @@
 import styled from 'styled-components';
 import { IAdvice } from '../../App';
-import diceImg from '../../images/icon-dice.svg';
-import hrDesktop from '../../images/pattern-divider-desktop.svg';
-import hrMobile from '../../images/pattern-divider-mobile.svg';
 
 const StyledCard = styled.main`
   width: 40%;
@@ -59,7 +56,7 @@ const Advice = styled.blockquote`
   font-weight: 800;
 `;
 
-const Divider = styled.img`
+const Divider = styled.div`
   margin-bottom: 20px;
 `;
 
@@ -72,10 +69,37 @@ export const Card: React.FC<{ advice: IAdvice; fetchNewAdvice: () => void }> = (
       <Header>Advice # {id}</Header>
       <Advice>&ldquo;{advice}&rdquo;</Advice>
 
-      <Divider src={window.innerWidth < 500 ? hrMobile : hrDesktop} alt="" />
+      <Divider>
+        {window.innerWidth < 500 ? (
+          <svg width="444" height="16" xmlns="http://www.w3.org/2000/svg">
+            <g fill="none" fill-rule="evenodd">
+              <path fill="#4F5D74" d="M0 8h196v1H0zM248 8h196v1H248z" />
+              <g transform="translate(212)" fill="#CEE3E9">
+                <rect width="6" height="16" rx="3" />
+                <rect x="14" width="6" height="16" rx="3" />
+              </g>
+            </g>
+          </svg>
+        ) : (
+          <svg width="295" height="16" xmlns="http://www.w3.org/2000/svg">
+            <g fill="none" fill-rule="evenodd">
+              <path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z" />
+              <g transform="translate(138)" fill="#CEE3E9">
+                <rect width="6" height="16" rx="3" />
+                <rect x="14" width="6" height="16" rx="3" />
+              </g>
+            </g>
+          </svg>
+        )}
+      </Divider>
 
       <DiceButton onClick={props.fetchNewAdvice}>
-        <img src={diceImg} alt="Click to generate a random advice" />
+        <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z"
+            fill="#202733"
+          />
+        </svg>
       </DiceButton>
     </StyledCard>
   );
